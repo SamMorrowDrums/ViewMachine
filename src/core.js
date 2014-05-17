3bc0f181f193689fe151adbf4a6690769e62a8be
Sam Morrow <info@sammorrowdrums.com>
2014-05-17 20:43:09 +0100

    Initialization of elements happens in superior fashion now

--- src/core.js
+++ src/core.js
@@ -2,9 +2,9 @@ define([
 ],
 function (viewMachine) {
   viewMachine = function (element, properties, style) {
-    if (this instanceof(viewMachine) === false) {
-      return new viewMachine(element, properties, style);
-    }
+    return new viewMachine.init(element, properties, style);
+  };
+  viewMachine.init = function (element, properties, style) {
     this.element = element;
     this.$ = document.createElement(element);
     if (properties === undefined) {
@@ -22,6 +22,7 @@ function (viewMachine) {
     this.type = 'ViewMachine';
     return this;
   };
+  viewMachine.init.prototype = viewMachine.prototype;
   window.viewMachine = window.VM = viewMachine;
   return viewMachine;
 });
