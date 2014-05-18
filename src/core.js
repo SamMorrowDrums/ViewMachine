a585a72bb61dd7f1cdfe513e675b1f52b04b3c1b
Sam Morrow <info@sammorrowdrums.com>
2014-05-18 09:01:04 +0100

    Enableing DOM elements to be arg for viemachine

--- src/core.js
+++ src/core.js
@@ -1,29 +1,39 @@
 define([
 ],
 function (viewMachine) {
+  
   viewMachine = function (element, attrs, style) {
     return new viewMachine.init(element, attrs, style);
   };
+
   viewMachine.init = function (element, attrs, style) {
-    var $ = element.toLowerCase() !== 'body' ? document.createElement(element) : document.getElementsByTagName('body')[0];
-    this.$ = $;
+    var $;
+    if (element instanceof HTMLElement === true) {
+      $ = element;
+      if ($.VM !== undefined) return $.VM;
+    } else {
+      $ = element.toLowerCase() !== 'body' ? document.createElement(element) : document.body;
+      this.$ = $;
+    }
     $.VM = this;
     this.element = $.tagName;
     if (attrs === undefined) {
       attrs = {};
     }
-    if (attrs.id !== null) {
+    if (attrs.id !== undefined) {
       $.id = attrs.id;
     }
     this.attrs = attrs;
     this.events = [];
     this.style = {};
-    this.type = 'ViewMachine';
     return this;
   };
-  if (window.onload === null) {window.onload = function() {viewMachine.body = viewMachine('body'); };
-  } else if (document.onload === null) {document.onload = function() {viewMachine.body = viewMachine('body'); }; }
+
+  if (window.onload === null) {window.onload = function() {viewMachine.body = viewMachine(document.body); };
+  } else if (document.onload === null) {document.onload = function() {viewMachine.body = viewMachine(document.body); }; }
   viewMachine.init.prototype = viewMachine.prototype;
   window.viewMachine = window.VM = viewMachine;
+  
   return viewMachine;
+
 });
