ba1b6905329f253b58ca1d2fcf2a72782af67eb6
Sam Morrow <info@sammorrowdrums.com>
2014-07-04 11:06:42 +0100

    Adding new dist files

--- dist/viewmachine.js
+++ dist/viewmachine.js
@@ -224,19 +224,14 @@
   };
 
   viewMachine.prototype.addClass = function (cl) {
-    var classes = this.$.getAttribute('class'),
+    var classes = this.$.getAttribute('class') || '',
     split;
     // If existing classes, add gracefully to end
 
-    if (classes && classes.indexOf(cl) < 0) {
+    if (classes.indexOf(cl) < 0) {
       split = classes.split(' ');
       split.push(cl);
       this.$.setAttribute('class', split.join(' '));
-    } else {
-
-      // Else set class equal to new class
-
-      this.$.setAttribute('class', cl);
     }
     return this;
   };
